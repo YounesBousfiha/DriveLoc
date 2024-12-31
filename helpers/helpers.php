a3fc84abeb7fe332607c777e865eed33361b9e00
Coderyounes <younesanas2021@gmail.com>
2024-12-31 16:49:58 +0100

    feat: #19 creation d'une function auxiliary for gerer les redirection

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -5,5 +5,9 @@ class Helpers {
     public static function generateToken() {
         return bin2hex(random_bytes(32));
     }
+
+    public static function redirect($url) {
+        return header("Location:" . $url);
+    }
 }
 ?>
