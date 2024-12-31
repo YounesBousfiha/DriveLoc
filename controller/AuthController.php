0479069856152b38db6b361ed0c85bbffbd83453
Coderyounes <younesanas2021@gmail.com>
2024-12-31 15:23:00 +0100

    feat: #19 Creation d'une function de inscription

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -25,9 +25,34 @@ class Auth {
         return $isFound;
     }
 
+    public function signup($newuserInstance) {
+        $isExist = $this->isExist($newuserInstance->__get('email'));
+        if(!$isExist) {
+            $sql = "INSERT INTO Users (nom, prenom, email, password, fk_role_id) VALUES (:nom, :prenom, :email, :password, :fk_role_id)";
+            $hashed_password = password_hash($newuserInstance->__get('password'), PASSWORD_DEFAULT);
+            try {
+                $stm = $this->db->prepare($sql);
+                $stm->bindValue(':Nom', $newuserInstance->__get('nom'));
+                $stm->bindValue(':Prenom', $newuserInstance->__get('prenom'));
+                $stm->bindValue(':Email', $newuserInstance->__get('email'));
+                $stm->bindValue(':Password', $hashed_password);
+                $stm->bindValue(':Id_role', $newuserInstance->__get('role'));
+                if($stm->execute()) {
+                    header("Location: LOGIN_PAGE"); 
+                    return true;
+                }
+            } catch (Exception $e) {
+                return "Error: " . $e->getMessage();
+            }
+        } else {
+            return "Email Already Used, Choose Another Address Email.";
+        }
+    }
+
     public function login() {}
-    public function signup() {}
+
     public function validateUser() {}
+    public function isLoggedIn() {}
     public function logout() {}
 }
 
