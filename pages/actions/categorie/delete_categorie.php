238aa23f8b46574d9936f6ec9fc32dfed2d7b375
Coderyounes <younesanas2021@gmail.com>
2025-01-03 11:27:04 +0100

    feat: functionality pour delete les categories

--- pages/actions/categorie/delete_categorie.php
+++ pages/actions/categorie/delete_categorie.php
@@ -6,12 +6,31 @@ use Younes\DriveLoc\Config\DBConnection;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
+$db = DBConnection::getConnection()->conn;
+
+$admin = new AdminController($db);
+
+$categorieData = $admin->getCategorie($_GET['id']);
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $db = DBConnection::getConnection()->conn;
-
-    $admin = new AdminController($db);
     $status = $admin->deleteCategorie($_POST['id']);
 
     echo $status;
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Categorie</title>
+</head>
+<body>
+<form action="delete_categorie.php?id=<?php echo $categorieData['categorie_id']; ?>" method="post">
+    <input type="hidden" name="id" value="<?php echo $categorieData['categorie_id']; ?>">
+    <input type="submit" value="Delete Categorie">
+</form>
+</body>
+</html>
