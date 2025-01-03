50b4f6ffb68a6455a814572939fd5c625c048068
Coderyounes <younesanas2021@gmail.com>
2025-01-03 15:06:45 +0100

    feat: le client peut fait des reervation maintenant

--- Controller/ReservationController.php
+++ Controller/ReservationController.php
@@ -2,7 +2,50 @@
 
 namespace Younes\DriveLoc\Controller;
 
-class ReservationController
+trait ReservationController
 {
+    private $db;
+    private $tableReservation = 'reservation';
+
+    public function approuverReservation($id)
+    {
+        $query = "UPDATE $this->tableReservation SET reservation_status = 'approuver' WHERE reservation_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function rejectReservation($id)
+    {
+        $query = "UPDATE $this->tableReservation SET reservation_status = 'rejeter' WHERE reservation_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function createReservation($data)
+    {
+        $columns = implode(",", array_keys(get_object_vars($data)));
+        $placeholders = ":" . implode(", :", array_keys(get_object_vars($data)));
+        $sql = "INSERT INTO {$this->tableReservation} ({$columns}) VALUES ({$placeholders})";
+        $stmt = $this->db->prepare($sql);
+
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(":{$key}", $value);
+        }
+
+        return $stmt->execute();
+    }
+
+    public function getAllReservations()
+    {
+        $sql = "SELECT * FROM {$this->tableReservation}";
+        $stmt = $this->db->prepare($sql);
+        if($stmt->execute()) {
+            return $stmt->fetchAll();
+        } else {
+            return null;
+        }
+    }
 
 }
