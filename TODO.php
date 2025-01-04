5ec72246b2bc518713849636848150fba0367183
Coderyounes <younesanas2021@gmail.com>
2025-01-04 13:13:45 +0100

    feat: finalization de tout les TODO

--- TODO.php
+++ TODO.php
@@ -1,12 +1,2 @@
 <?php
-
-
-// TODO : add new column into Avis  is_delete to implement the Soft Delete
-// TODO : in the Avis getAll add Condition to display only the is_delete null/false
-// TODO : in the Avis delete add the update query to set is_delete to true
-// TODO: in Categorie table add new column fk_user_id to know who create the categorie
-// TODO: in Client Management implement soft delete
-
-
-// TODO:
 ?>
